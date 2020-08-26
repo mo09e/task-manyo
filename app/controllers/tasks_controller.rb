6c3c484571e38ec77a574967ea1c4be013760b9b
Moe <moe_akahane@diveintocode.jp>
2020-08-26 19:43:56 +0900

    Add:indexとshowのController、Viewを記述

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,19 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
   def index
+    @task = Task.all
   end
 
   def show
+  end
+
+  private
+  def task_params
+    params.require(:myword).permit(:task_name, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
