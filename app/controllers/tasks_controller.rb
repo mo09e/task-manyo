763b374612ff8092ae04f5db962c27a293869f82
Moe <moe_akahane@diveintocode.jp>
2020-08-26 20:12:17 +0900

    Add:tasks controllerに投稿、編集、投稿の際に確認ができるアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,48 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show]
+  before_action :set_task, only: [:show, :edit, :update]
 
   def index
-    @task = Task.all
+    @tasks = Task.all
+  end
+
+  def new
+    if params[:back]
+      @task = Task.new(task_params)
+    else
+      @task = Task.new
+    end
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice: "タスクを投稿しました"
+      else
+        render :new
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "'タスクを編集しました'"
+    else
+      render :edit
+    end
   end
 
   def show
+  end
+
+  def confirm
+    @task = Task.new(task_params)
+    render :new if @task.invalid?
   end
 
   private
