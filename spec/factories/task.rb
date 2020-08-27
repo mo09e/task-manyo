310cae688c601a7398fea0172b3c3a2a5f957aab
Moe <moe_akahane@diveintocode.jp>
2020-08-27 15:53:19 +0900

    Add:SystemSpecテスト３件のための記述を追加、ブラウザ表示のためにspec_helper.rbに記述を追加

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -3,4 +3,8 @@ FactoryBot.define do
     task_name { 'test_task_name' }
     content { 'test_content' }
   end
+  factory :second_task, class: Task do
+    task_name { 'Sample_task2' }
+    content { 'take a work' }
+  end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-        task = FactoryBot.create(:task, task_name: 'task')
-        visit tasks_path
+        visit new_task_path
+        fill_in :task_task_name, with: 'task'
+        fill_in :task_content, with: 'content'
+        click_on "Post task"
         expect(page).to have_content 'task'
       end
     end
@@ -12,12 +18,21 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
+        task = FactoryBot.create(:task, task_name: 'task', content: 'iroha')
+        visit tasks_path
+        current_path
+        Task.count
+        page.html
+        expect(page).to have_content 'task'
       end
     end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
+         task = FactoryBot.create(:second_task)
+         visit task_path(task.id)
+         expect(page).to have_content 'take a work'
        end
      end
   end
