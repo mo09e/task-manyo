e9cb8ca7c682f41faefee99f794837da70cdf118
Moe <moe_akahane@diveintocode.jp>
2020-08-28 13:58:33 +0900

    Add:タスク一覧の降順表示のためのテストを記述

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task, task_name: 'task', content: 'swim') }
   before do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
+    visit tasks_path
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -24,6 +26,12 @@ RSpec.describe 'タスク管理機能', type: :system do
         Task.count
         page.html
         expect(page).to have_content 'task'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'task'
       end
     end
   end
