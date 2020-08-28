82eced257fbb1562b1cd820220652d593837bb1f
Moe <moe_akahane@diveintocode.jp>
2020-08-28 18:37:23 +0900

    Add:Tasksテーブルに終了期限のカラムを追加。Null制約、デフォルト値も付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_091105) do
+ActiveRecord::Schema.define(version: 2020_08_28_091508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_26_091105) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "deadline", null: false
   end
 
 end
