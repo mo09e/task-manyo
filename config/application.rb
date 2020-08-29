0b9dd59b55cffb91ed6253fce313d5f0c781e56f
Moe <moe_akahane@diveintocode.jp>
2020-08-29 13:09:58 +0900

    Change:終了期限の日時の表示方法を変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TaskManyo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     config.active_record.default_timezone = :local
     config.load_defaults 5.2
 
