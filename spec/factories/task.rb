f376cd5575482183c7a07c04c4765c4acafc0caa
Moe <moe_akahane@diveintocode.jp>
2020-08-31 16:10:30 +0900

    Add:終了期限ソートのためのテストコードを記述

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,10 +1,19 @@
 FactoryBot.define do
   factory :task do
-    task_name { 'test_task_name' }
+    task_name { 'sample_name' }
     content { 'test_content' }
+    deadline { '2020-08-29 13:40:00' }
   end
+
   factory :second_task, class: Task do
     task_name { 'Sample_task2' }
     content { 'take a work' }
+    deadline { '2020-08-30 13:40:00' }
+  end
+
+  factory :third_task, class: Task do
+    task_name { 'Sample_task3' }
+    content { 'Eat lunch' }
+    deadline { '2020-08-31 15:00:00' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,31 +1,46 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task) { FactoryBot.create(:task, task_name: 'task', content: 'swim') }
+  # let!(:task) { FactoryBot.create(:task, task_name: 'task', content: 'swim') }
   before do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
+    FactoryBot.create(:third_task)
     visit tasks_path
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in :task_task_name, with: 'task'
-        fill_in :task_content, with: 'content'
+        fill_in :task_task_name, with: 'task_name'
+        fill_in :task_content, with: 'task'
+        save_and_open_page
+        select '2021', from: 'Deadline'
+        save_and_open_page
         click_on "Post task"
         expect(page).to have_content 'task'
+        expect(page).to have_select('Deadline', selected: '2021')
       end
     end
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, task_name: 'task', content: 'iroha')
+        task = FactoryBot.create(:task, task_name: 'task', content: 'iroha', deadline: '2020-09-01 16:00:00')
         visit tasks_path
         current_path
         Task.count
         page.html
         expect(page).to have_content 'task'
+      end
+    end
+    context '終了期日でソートした場合' do
+      it 'タスクが終了期日の降順に並んでいること' do
+        # click_on "終了期限でソートをかける"
+        # find("#sort_by_deadline").click
+        visit tasks_path(sort_expired: "false")
+        task_list = all('.task_row_deadline')
+        expect(task_list[0]).to have_content '2020/08/31'
+        expect(task_list[1]).to have_content '2020/08/30'
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
