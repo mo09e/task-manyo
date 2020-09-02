d83109c1fdf76d2bf11d0c0fd7ff3edd4df72114
Moe <moe_akahane@diveintocode.jp>
2020-09-02 11:23:07 +0900

    Add:task_nameカラムにindexを追加

---
+++ db/migrate/20200902022059_add_index_to_tasks_task_name.rb
@@ -0,0 +1,5 @@
+class AddIndexToTasksTaskName < ActiveRecord::Migration[5.2]
+  def change
+    add_index :tasks, :task_name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_013259) do
+ActiveRecord::Schema.define(version: 2020_09_02_022059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_013259) do
     t.datetime "updated_at", null: false
     t.datetime "deadline", null: false
     t.string "status", null: false
+    t.index ["task_name"], name: "index_tasks_on_task_name"
   end
 
 end
