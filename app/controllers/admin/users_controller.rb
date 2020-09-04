eb4001eb7bed3dfc926179f1d0c03d483fa9568e
Moe <moe_akahane@diveintocode.jp>
2020-09-04 14:43:29 +0900

    Add:作成、編集、詳細のViewを作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,7 +13,7 @@ class Admin::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @task.save
+    if @user.save
       redirect_to admin_users_path, notice: t('view.models.admin.new')
     else
       render :new
