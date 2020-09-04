1a38c5dd7183b01e12568831e32c30fabcfefaea
Moe <moe_akahane@diveintocode.jp>
2020-09-04 13:40:56 +0900

    Add:管理画面用のコントローラに作成、編集、削除機能を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,57 @@
 class Admin::UsersController < ApplicationController
+  before_action :admin_user
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all.order("created_at DESC")
+    @users = @users.page(params[:page]).per(10)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @task.save
+      redirect_to admin_users_path, notice: t('view.models.admin.new')
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: t('view.models.admin.edit')
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @user.Destroy
+    redirect_to admin_users_path, notice: t('view.models.admin.destroy')
+  end
+
+  private
+  def admin_user
+    if current_user.admin == false
+      flash[:notice] = t("msg.not_authorized")
+      redirect_to(tasks_path)
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
- resources :users, module: 'admin'
- resources :users, only: [:new, :create, :show]
- resources :sessions, only: [:new, :create, :destroy]
- resources :tasks do
-   collection do
-     post :confirm
-   end
- end
+  namespace :admin do
+    resources :users
+  end
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :tasks do
+    collection do
+      post :confirm
+    end
+  end
 
- root to: "session#new"
+  root to: "sessions#new"
 end
