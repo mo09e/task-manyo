620ef92ead04b5ae8a41eddfec8f3642d85f0c97
Moe <moe_akahane@diveintocode.jp>
2020-09-08 10:21:35 +0900

    Fix:テストコード一部を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
     name { "daVinchi" }
     email { "monalisa@sugoi.jp" }
     password { "leonardo" }
-    admin { 'true' }
+    admin { true }
   end
 
   factory :second_user , class: User do
     name { "Michelangelo" }
     email { "pieta@sugoi.jp" }
     password { "virginmary" }
-    admin { 'false' }
+    admin { false }
   end
 end
